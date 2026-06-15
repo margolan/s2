c4e1acb3d5e1bed54c43db07cae1a8be0eabc442
margulan <margulan@0x0.kz>
2026-06-15 15:56:36 +0500

    wip

--- routes/custom.php
+++ routes/custom.php
@@ -21,66 +21,66 @@ Route::get('/test', [ServiceController::class, 'test']);
 
 
 
-// Route::domain('0x0.kz')->group(function () {
+Route::domain('0x0.kz')->group(function () {
 
-// ====================== COMMON  ======================
+  // ====================== COMMON  ======================
 
-Route::get('', [IndexController::class, 'index'])->name('index');
-Route::get('/about', [IndexController::class, 'about'])->name('about');
-Route::get('/help', [IndexController::class, 'help'])->name('help');
+  Route::get('', [IndexController::class, 'index'])->name('index');
+  Route::get('/about', [IndexController::class, 'about'])->name('about');
+  Route::get('/help', [IndexController::class, 'help'])->name('help');
 
 
 
-// ====================== ADMIN DASHBOARD ======================
+  // ====================== ADMIN DASHBOARD ======================
 
-Route::middleware('auth')->group(function () {
-  Route::get('/dashboard/admin', [AdminController::class, 'index'])->name('admin-dashboard');
+  Route::middleware('auth')->group(function () {
+    Route::get('/dashboard/admin', [AdminController::class, 'index'])->name('admin-dashboard');
+  });
+
+
+
+  // ====================== SCHEDULES PROJECT ======================
+
+  Route::get('/grafik', [ScheduleController::class, 'index'])->name('schedule-index');
+  Route::post('/grafik', [ScheduleController::class, 'settings'])->name('schedule-settings');
+
+  Route::middleware('CheckUser:ter,pos')->group(function () {
+    Route::get('/dashboard/schedule', [ScheduleController::class, 'dashboard'])->name('schedule-dashboard');
+    Route::post('/dashboard/schedule/store', [ScheduleController::class, 'store'])->name('schedule-store');
+    Route::put('/dashboard/schedule/activate', [ScheduleController::class, 'activate'])->name('schedule-activate');
+    Route::delete('/dashboard/schedule/delete', [ScheduleController::class, 'delete'])->name('schedule-delete');
+  });
+
+
+
+  // ====================== KEYS PROJECT ======================
+
+  Route::match(['get', 'post'], '/dashboard/key/pincode', [KeyController::class, 'pincode'])->name('key-pincode');
+
+  Route::middleware('CheckUser:ter')->group(function () {
+    Route::get('/dashboard/key', [KeyController::class, 'dashboard'])->name('key-dashboard');
+    Route::post('/dashboard/key/store', [KeyController::class, 'store'])->name('key-store');
+    Route::match(['get', 'put'], '/dashboard/key/edit', [KeyController::class, 'edit'])->name('key-edit');
+    Route::delete('/dashboard/key/delete', [KeyController::class, 'delete'])->name('key-delete');
+  });
+
+
+
+  // ====================== CASSETTE PROJECT ======================
+
+  Route::middleware('CheckUser:upr')->group(function () {
+    Route::match(['get', 'post'], '/dashboard/cassette', [CassetteController::class, 'dashboard'])->name('cassette-dashboard');
+    Route::delete('/cassette/delete', [CassetteController::class, 'delete'])->name('cassette-delete');
+  });
+
+
+
+
+  // ====================== SERVICE ROUTES ======================
+
+
+  // Route::get('/test', [ServiceController::class, 'test']);
 });
-
-
-
-// ====================== SCHEDULES PROJECT ======================
-
-Route::get('/grafik', [ScheduleController::class, 'index'])->name('schedule-index');
-Route::post('/grafik', [ScheduleController::class, 'settings'])->name('schedule-settings');
-
-Route::middleware('CheckUser:ter,pos')->group(function () {
-  Route::get('/dashboard/schedule', [ScheduleController::class, 'dashboard'])->name('schedule-dashboard');
-  Route::post('/dashboard/schedule/store', [ScheduleController::class, 'store'])->name('schedule-store');
-  Route::put('/dashboard/schedule/activate', [ScheduleController::class, 'activate'])->name('schedule-activate');
-  Route::delete('/dashboard/schedule/delete', [ScheduleController::class, 'delete'])->name('schedule-delete');
-});
-
-
-
-// ====================== KEYS PROJECT ======================
-
-Route::match(['get', 'post'], '/dashboard/key/pincode', [KeyController::class, 'pincode'])->name('key-pincode');
-
-Route::middleware('CheckUser:ter')->group(function () {
-  Route::get('/dashboard/key', [KeyController::class, 'dashboard'])->name('key-dashboard');
-  Route::post('/dashboard/key/store', [KeyController::class, 'store'])->name('key-store');
-  Route::match(['get', 'put'], '/dashboard/key/edit', [KeyController::class, 'edit'])->name('key-edit');
-  Route::delete('/dashboard/key/delete', [KeyController::class, 'delete'])->name('key-delete');
-});
-
-
-
-// ====================== CASSETTE PROJECT ======================
-
-Route::middleware('CheckUser:upr')->group(function () {
-  Route::match(['get', 'post'], '/dashboard/cassette', [CassetteController::class, 'dashboard'])->name('cassette-dashboard');
-  Route::delete('/cassette/delete', [CassetteController::class, 'delete'])->name('cassette-delete');
-});
-
-
-
-
-// ====================== SERVICE ROUTES ======================
-
-
-// Route::get('/test', [ServiceController::class, 'test']);
-// });
 
 
 
@@ -97,9 +97,4 @@ Route::domain('aper.kz')->group(function () {
 
     return view('aperkz.index');
   });
-
-  Route::any('{any}', function () {
-
-    abort(404);
-  })->where('any', '.*');
 });
